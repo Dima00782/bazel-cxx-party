a4c3c1fee73228579223f86803e77167edffde40
Dmitry Bezhetskov <dima00782@gmail.com>
2019-04-23 09:24:33 +0700

    add protocol buffers

--- src/lib/cxx_lib.cc
+++ src/lib/cxx_lib.cc
@@ -2,8 +2,21 @@
 
 #include <iostream>
 
+#include "lib/person.pb.h"
+
+demo::Person GetSomePerson() {
+    demo::Person person;
+
+    person.set_id(0);
+    person.set_name("noname");
+    person.set_email("some@yandex-team.ru");
+
+    return person;
+}
+
 void SayHello() {
     std::cout << "Hi C++ Party" << std::endl;
+    std::cout << GetSomePerson().DebugString() << std::endl;
 }
 
 int Square(int x) {
